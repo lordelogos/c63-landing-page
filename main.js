f499241f043e0503c196b72b37c02f026f7a71d8
Paul OE <paulehiks@gmail.com>
2022-02-14 19:38:34 +0100

    animation added

--- main.js
+++ main.js
@@ -9,17 +9,34 @@ let flkty = new Flickity( elem, {
 //   contain: true
 });
 
-
-// initializing Gsap
-
 // register scrollTrigger plugini with gsap
 gsap.registerPlugin(ScrollTrigger);
 
+// toggle nav on mobile
+let nav = document.querySelector('.header .menu')
+let menuOpen = document.querySelector('#open-btn')
+let menuClose = document.querySelector('#close-btn')
+
+const openMenu = e => {
+  nav.classList.add('active-menu')
+}
+
+const closeMenu = e => {
+  nav.classList.remove('active-menu')
+}
+
+menuOpen.addEventListener('click', openMenu)
+
+menuClose.addEventListener('click', closeMenu)
+
+//animate the mobile nav
+
+//animation for desktop
 let tl = gsap.timeline({ defaults: {ease: "power4.inOut", duration: 2}})
 
 tl.to('.banner-infotext', {y: 0, clipPath: "polygon(100% 100%, 0% 100%, 0% 0%, 100% 0%)", opacity: 1})
 .to('.banner-infocta', {y: 0, clipPath: "polygon(100% 100%, 0% 100%, 0% 0%, 100% 0%)", opacity: 1}, "-=1.6")
-.to('.banner-stats li', {y: 0, clipPath: "polygon(100% 100%, 0% 100%, 0% 0%, 100% 0%)", opacity: 1, stagger: .1}, "-=1.8")
+.to('.banner-stats li', {y: 0, clipPath: "polygon(100% 100%, 0% 100%, 0% 0%, 100% 0%)", opacity: 1, stagger: .2}, "-=1.8")
 
 
 // animating the showcase section
@@ -55,3 +72,8 @@ let td_tl = gsap.timeline({defaults : {ease: 'power4.inOut', duration: 1}, scrol
 td_tl.from('.exploretext', {opacity: 0, y: 100})
 .from('.testdrive img' ,{opacity: 0, x: -100,}, '-=.8')
 .from('.testdrive div', {opacity: 0, x: 100}, "-=1")
+
+//offers section
+let offer_tl = gsap.timeline({defaults: {ease: 'power4.inOut', duration: 1}, scrollTrigger: {trigger: '.offers', toggleActions: 'play none none none'}})
+
+offer_tl.from('.offers img, .offers div', {opacity: 0, transform: 'scale(0.1)', duration: 2, stagger: .5})
